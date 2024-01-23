81fa21e7f511c8c8d6d20996cc533ac98567267f
Des <Desmondmls@gmail.com>
2024-01-23 19:08:36 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const sum = function (a, b) {
+  return a + b
+}
+sum(1, 2)
+
+const minus = function (a, b) {
+  return a - b
+}
+minus(2, 1)
+const mul = function (a, b) {
+  return a * b
+}
+mul(1, 2)
